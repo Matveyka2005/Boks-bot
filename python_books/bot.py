b106937717245560c4c0b9cdb62c12654886fba9
Matvey <matveykawork@gmail.com>
2023-04-12 11:48:31 +0300

    n

--- python_books/bot.py
+++ python_books/bot.py
@@ -3,6 +3,8 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+import message_texts
+
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
@@ -11,13 +13,35 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+logger = logging.getLogger(__name__)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("effective_caht is None")
+        return 
+    await context.bot.send_message(
+        chat_id=effective_chat.id,
+        text=message_texts.GREETINGS
+        )
+    
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("effective_caht is None")
+        return 
+    await context.bot.send_message(
+        chat_id=effective_chat.id,
+        text=message_texts.HELP
+        )
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     
     start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    start_handler = CommandHandler('help', help)
     application.add_handler(start_handler)
     
     application.run_polling()
